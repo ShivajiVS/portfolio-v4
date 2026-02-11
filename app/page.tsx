bcc4d8a476d5dd0eebad2d085b615013db635731
kondeti shivaji <shivajikondeti555@gmail.com>
2026-02-11 14:16:49 +0530

    add Back Soon

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,12 @@ import { Button } from '@/components/ui/button';
 export default function Home() {
   return (
     <div>
-      <Button> Click me</Button>
+      <div className='flex h-screen w-screen items-center justify-center '>
+        <div className=' space-y-1'>
+          <h1 className='font-bold text-xl capitalize italic'>Back Soon</h1>
+          <h3 className='font-medium text-lg capitalize italic'>Refactoring the experience. 🚀</h3>
+        </div>
+      </div>
     </div>
   );
 }
